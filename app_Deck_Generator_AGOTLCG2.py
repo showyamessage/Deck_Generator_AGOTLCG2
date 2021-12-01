a751226a9929511fbc07f11bb0d1c5cf819cce54
showyamessage <95348379+showyamessage@users.noreply.github.com>
2021-12-01 12:29:21 +0100

    Update app_Deck_Generator_AGOTLCG2.py

--- app_Deck_Generator_AGOTLCG2.py
+++ app_Deck_Generator_AGOTLCG2.py
@@ -261,10 +261,10 @@ st.title("Decklist Generator" + "  \n"  + "for A Game of Thrones LCG 2nd Edition
 # =============================================================================
 # EINMALIG!!! load Decks, cards, packs, restricted Lists from .json file
 # =============================================================================
-alldecks = pd.read_json("C:/Users/braun/Documents/Thrones/streamlit/Deck_Generator_AGOTLCG2/Decks.json")
-allcards = pd.read_json("C:/Users/braun/Documents/Thrones/streamlit/Deck_Generator_AGOTLCG2/AllCards.json", dtype={'code': 'string'})
-allpacks = pd.read_json("C:/Users/braun/Documents/Thrones/streamlit/Deck_Generator_AGOTLCG2/AllPacks.json")
-allRL = pd.read_json("C:/Users/braun/Documents/Thrones/streamlit/Deck_Generator_AGOTLCG2/RestrictedLists.json") 
+alldecks = pd.read_json("Decks.json")
+allcards = pd.read_json("AllCards.json", dtype={'code': 'string'})
+allpacks = pd.read_json("AllPacks.json")
+allRL = pd.read_json("RestrictedLists.json") 
 
 # ADD 'available' to get releasedate for cards -> further steps more easy
 allcards = pd.merge(allcards, allpacks[['code', 'available']], left_on='pack_code', right_on='code', how="left", suffixes=('', '_y'))
