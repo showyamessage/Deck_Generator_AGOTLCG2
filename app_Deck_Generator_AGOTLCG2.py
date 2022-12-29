02cafc8afbdd98ae92fb0f1000642e0710e065f6
showyamessage <95348379+showyamessage@users.noreply.github.com>
2022-12-29 11:03:42 +0100

    Update app_Deck_Generator_AGOTLCG2.py

--- app_Deck_Generator_AGOTLCG2.py
+++ app_Deck_Generator_AGOTLCG2.py
@@ -307,6 +307,13 @@ if AndOr == "AND":
     for strCardlabel in lstCardlabels: #"slots": {"01002": 1, ... }
         curr_code =  allcards[allcards['label'] == strCardlabel].iloc[0]['code'] #[x["code"] for x in allcards if x["label"] == strCardlabel][0]
         alldecks = alldecks[alldecks.apply(lambda row: curr_code in row["slots"].keys(), axis=1)]
+elseif AndOr == "OR":
+    k = 0
+    for strCardlabel in lstCardlabels:
+        curr_code =  allcards[allcards['label'] == strCardlabel].iloc[0]['code'] #[x["code"] for x in allcards if x["label"] == strCardlabel][0]
+        decksForCard[k] = alldecks[alldecks.apply(lambda row: curr_code in row["slots"].keys(), axis=1)]
+        k += 1 
+    alldecks = pd.concat(decksForCard)
 with col3: st.info(str(len(alldecks)) + " Decks found." + "  \n" +  "Recommended: <100 (avoid long runtime)") #st.markdown('<p class="big-font">'+ "Decks: " + str(len(alldecks))  + '</p>', unsafe_allow_html=True)
         
 # =============================================================================
